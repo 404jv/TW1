a3e3dc731037c0ad6da80c417f84ed1cc26ba4cf
João Victor <joaovictorramalho7@gmail.com>
2021-08-23 16:54:40 -0300

    Create user controller

---
+++ biblioteca/br/edu/ifpr/controllers/UserController.java
@@ -0,0 +1,27 @@
+package br.edu.ifpr.controllers;
+
+import javax.swing.JOptionPane;
+
+import br.edu.ifpr.repositories.UserRepository;
+
+public class UserController {
+  UserRepository userRepository;
+
+  public UserController() {
+    this.userRepository = UserRepository.getInstance();
+  }
+
+  public void create() {
+    String nome = JOptionPane.showInputDialog("Digite o nome");
+
+    String email = JOptionPane.showInputDialog("Digite o email, do(a) " + nome);
+
+    String senha = JOptionPane.showInputDialog("Digite uma senha para o(a) " + nome);
+
+    String idade = JOptionPane.showInputDialog("Qual é a idade do(a) " + nome + "?");
+
+    String curso = JOptionPane.showInputDialog("E o curso dele(a)?");
+
+    this.userRepository.create(nome, email, senha, idade, curso);
+  }
+}
